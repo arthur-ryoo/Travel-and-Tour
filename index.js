784542328455fe219f8c38c76a28fa2b51be4e69
ArthurRyoo <jene0523@gmail.com>
2021-02-22 23:11:45 -0800

    Set database connection

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+const mongoose = require('mongoose')
+const db = mongoose.connection;
+require('dotenv').config();
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useCreateIndex: true, 
+    useFindAndModify: false
+});
+
+db.once('connected', () => {
+    console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+  });
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Express app running on port ${port}`))
