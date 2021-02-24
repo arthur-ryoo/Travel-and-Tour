2e141b27e12b16d5300f9d263a2e76bf16c6502f
ArthurRyoo <jene0523@gmail.com>
2021-02-23 23:17:00 -0800

    Add register route

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const config = require('./config/key');
+
 const mongoose = require('mongoose')
 const db = mongoose.connection;
-require('dotenv').config();
 
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
     useCreateIndex: true, 
@@ -16,7 +23,17 @@ db.once('connected', () => {
     console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
   });
 
-
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+    
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Express app running on port ${port}`))
