48dc7ef512d8a5779700ba3e64224ce7ef2d08dc
ArthurRyoo <jene0523@gmail.com>
2021-02-24 22:31:46 -0800

    Finish login and logout APIs

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const { User } = require('./models/User');
+const { auth } = require('./middleware/auth');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const config = require('./config/key');
 
@@ -25,12 +28,59 @@ db.once('connected', () => {
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.post('/register', (req, res) => {
-    
+app.post('/api/users/register', (req, res) => {
     const user = new User(req.body)
-    user.save((err, userInfo) => {
+    user.save((err, user) => {
         if (err) return res.json({ success: false, err})
         return res.status(200).json({
+            success: true
+        })
+    })
+})
+
+app.post('/api/users/login', (req, res) => {
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "bad credentials"
+            })
+        }
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(isMatch) {
+                user.generateToken((err, user) => {
+                    if (err) return res.status(400).send(err);
+                        res.cookie("x_auth", user.token)
+                        .status(200)
+                        .json({ loginSuccess: true, userId: user._id})
+                })
+            } else {
+                return res.json({ loginSuccess: false, message: "bad credentials" })
+            }
+        })
+    })
+})
+
+app.get('/api/users/auth', auth, (req, res) => {
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastName: req.user.lastName,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate(
+        { _id: req.user._id }, { token: "" }, 
+        (err, user) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).send({
             success: true
         })
     })
