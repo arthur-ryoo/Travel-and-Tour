5e1af23c26510d403c93d4e7e2b2f0f7bd65601b
ArthurRyoo <jene0523@gmail.com>
2021-03-04 00:34:45 -0800

    Create landing page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
-function LandingPage() {
-    return (
-        <div>
-            LandingPage
-        </div>
-    )
+function LandingPage(props) {
+  const onClick = () => {
+    axios.get('/api/users/logout').then((response) => {
+      if (response.data.success) {
+        props.history.push('/login');
+      } else {
+        alert('Failed to log out');
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <h2>Landing Page</h2>
+      <button onClick={onClick}>Logout</button>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
