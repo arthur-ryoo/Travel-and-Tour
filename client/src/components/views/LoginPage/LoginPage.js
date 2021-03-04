1b87f42102eb1b15fb6331615252e9cb417fb963
ArthurRyoo <jene0523@gmail.com>
2021-03-04 00:39:25 -0800

    Create login page

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,11 +1,66 @@
-import React from 'react'
+import React, { useReducer } from 'react';
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../../_actions/user_action';
 
-function LoginPage() {
-    return (
-        <div>
-            LoginPage
-        </div>
-    )
+function LoginPage(props) {
+  const dispatch = useDispatch();
+  const [userInput, setUserInput] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    {
+      email: '',
+      password: '',
+    }
+  );
+
+  const handleChange = (event) => {
+    setUserInput({ [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let body = { email: userInput.email, password: userInput.password };
+    dispatch(loginUser(body)).then((response) => {
+      if (response.payload.loginSuccess) {
+        props.history.push('/');
+      } else {
+        alert('Falied to log in');
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <form
+        style={{ display: 'flex', flexDirection: 'column' }}
+        onSubmit={handleSubmit}
+      >
+        <label>Email</label>
+        <input
+          type="email"
+          name="email"
+          value={userInput.email}
+          onChange={handleChange}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          value={userInput.password}
+          onChange={handleChange}
+        />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
