5c32cada2f048769913daed89bb00eec908623e7
ArthurRyoo <jene0523@gmail.com>
2021-03-04 00:31:34 -0800

    Setup authentication

---
+++ client/src/hoc/auth.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { auth } from '../_actions/user_action';
+
+export default function authentication(Component, option, adminRoute = null) {
+  function AuthenticationCheck(props) {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+      dispatch(auth()).then((response) => {
+        console.log(response);
+        if (!response.payload.isAuth) {
+          if (option) {
+            props.history.push('/login');
+          }
+        } else {
+          if (adminRoute && !response.payload.isAdmin) {
+            props.history.push('/');
+          } else {
+            if (!option) {
+              props.history.push('/');
+            }
+          }
+        }
+      });
+    }, []);
+    return <Component {...props} />;
+  }
+
+  return AuthenticationCheck;
+}
