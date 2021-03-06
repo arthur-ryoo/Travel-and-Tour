b7cf03ee11f7826b40f7a0288cc84c0820785a93
ArthurRyoo <jene0523@gmail.com>
2021-03-05 17:32:04 -0800

    Add navbar and suspense

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,33 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { Suspense } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Auth from './hoc/auth';
 
 import LandingPage from './components/views/LandingPage/LandingPage';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
-import Auth from './hoc/auth';
+import NavBar from './components/views/NavBar/NavBar';
+import Footer from './components/views/Footer/Footer';
+import UploadProductPage from './components/views/UploadProductPage/UploadProductPage';
 
 function App() {
   return (
-    <Router>
-      <div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <NavBar />
+      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
         <Switch>
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route
+            exact
+            path="/product/upload"
+            component={Auth(UploadProductPage, true)}
+          />
         </Switch>
       </div>
-    </Router>
+      <Footer />
+    </Suspense>
   );
 }
 
