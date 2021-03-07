abcb999552b2ee684171b58e4ac6ff2fed2ac354
ArthurRyoo <jene0523@gmail.com>
2021-03-06 21:40:34 -0800

    Add footer component

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
-    return (
-        <div>
-            Footer
-        </div>
-    )
+  return (
+    <div
+      style={{
+        height: '80px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: '1rem',
+      }}
+    >
+      <footer>
+        <p>Travel and Tour &copy; {new Date().getFullYear()}</p>
+      </footer>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
