62b312455462d7307e7103a80d8ad272466008c1
ArthurRyoo <jene0523@gmail.com>
2021-03-08 11:09:18 -0800

    Create user route folder

--- server/index.js
+++ server/index.js
@@ -3,87 +3,30 @@ const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const { User } = require('./models/User');
-const { auth } = require('./middleware/auth');
+const cors = require('cors');
+
+const config = require('./config/key');
+
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+
+mongoose.connect(config.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-const config = require('./config/key');
-
-const mongoose = require('mongoose')
-const db = mongoose.connection;
-
-mongoose.connect(config.mongoURI, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-    useCreateIndex: true, 
-    useFindAndModify: false
+db.once('connected', () => {
+  console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
 });
 
-db.once('connected', () => {
-    console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
-  });
+app.use('/api/users', require('./routes/users'));
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.post('/api/users/register', (req, res) => {
-    const user = new User(req.body)
-    user.save((err, user) => {
-        if (err) return res.json({ success: false, err})
-        return res.status(200).json({
-            success: true
-        })
-    })
-})
-
-app.post('/api/users/login', (req, res) => {
-    User.findOne({ email: req.body.email }, (err, user) => {
-        if (!user) {
-            return res.json({
-                loginSuccess: false,
-                message: "bad credentials"
-            })
-        }
-
-        user.comparePassword(req.body.password, (err, isMatch) => {
-            if(isMatch) {
-                user.generateToken((err, user) => {
-                    if (err) return res.status(400).send(err);
-                        res.cookie("x_auth", user.token)
-                        .status(200)
-                        .json({ loginSuccess: true, userId: user._id})
-                })
-            } else {
-                return res.json({ loginSuccess: false, message: "bad credentials" })
-            }
-        })
-    })
-})
-
-app.get('/api/users/auth', auth, (req, res) => {
-    res.status(200).json({
-        _id: req.user._id,
-        isAdmin: req.user.role === 0 ? false : true,
-        isAuth: true,
-        email: req.user.email,
-        name: req.user.name,
-        lastName: req.user.lastName,
-        role: req.user.role,
-        image: req.user.image
-    })
-})
-
-app.get('/api/users/logout', auth, (req, res) => {
-    User.findOneAndUpdate(
-        { _id: req.user._id }, { token: "" }, 
-        (err, user) => {
-        if (err) return res.json({ success: false, err })
-        return res.status(200).send({
-            success: true
-        })
-    })
-})
-
-app.listen(port, () => console.log(`Express app running on port ${port}`))
+app.listen(port, () => console.log(`Express app running on port ${port}`));
