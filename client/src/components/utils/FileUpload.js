bcedcdde527587511c16238afde91710608c0481
ArthurRyoo <jene0523@gmail.com>
2021-03-09 22:05:06 -0800

    Add product upload page and feature

---
+++ client/src/components/utils/FileUpload.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import Dropzone from 'react-dropzone';
+import { PlusOutlined } from '@ant-design/icons';
+import axios from 'axios';
+
+function FileUpload(props) {
+  const [image, setImage] = useState([]);
+
+  const handleDrop = (files) => {
+    let formData = new FormData();
+    const config = {
+      header: { 'content-type': 'multipart/form-data' },
+    };
+    formData.append('file', files[0]);
+    axios.post('/api/products/image', formData, config).then((response) => {
+      if (response.status === 200) {
+        setImage([...image, response.data.imageUrl]);
+        props.handleImage([...image, response.data.imageUrl]);
+      } else {
+        alert('Failed to save the image');
+      }
+    });
+  };
+
+  const handleDelete = (selectedImage) => {
+    const selectedImageIndex = image.indexOf(selectedImage);
+    let newImages = [...image];
+    newImages.splice(selectedImageIndex, 1);
+    setImage(newImages);
+    props.handleImage(newImages);
+  };
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <Dropzone onDrop={handleDrop}>
+        {({ getRootProps, getInputProps }) => (
+          <div
+            style={{
+              width: 300,
+              height: 240,
+              border: '1px solid lightgray',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            {...getRootProps()}
+          >
+            <input {...getInputProps()} />
+            <PlusOutlined style={{ fontSize: '3rem' }} />
+          </div>
+        )}
+      </Dropzone>
+      <div
+        style={{
+          display: 'flex',
+          width: '350px',
+          height: '260px',
+          overflowX: 'auto',
+        }}
+      >
+        {image.map((i, index) => (
+          <div onClick={() => handleDelete(image)} key={index}>
+            <img
+              style={{ minWidth: '300px', width: '300px', height: '240px' }}
+              src={i}
+              alt="country"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default FileUpload;
--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,7 +1,134 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import FileUpload from '../../utils/FileUpload';
+import { Typography, Button, Form, Input } from 'antd';
+import axios from 'axios';
 
-function UploadProductPage() {
-  return <div>UploadProductPage</div>;
+const { Title } = Typography;
+const { TextArea } = Input;
+
+const Continents = [
+  {
+    key: 1,
+    value: 'Africa',
+  },
+  {
+    key: 2,
+    value: 'Europe',
+  },
+  {
+    key: 3,
+    value: 'Asia',
+  },
+  {
+    key: 4,
+    value: 'North America',
+  },
+  {
+    key: 5,
+    value: 'South America',
+  },
+  {
+    key: 6,
+    value: 'Australia',
+  },
+  {
+    key: 7,
+    value: 'Antarctica',
+  },
+];
+
+function UploadProductPage(props) {
+  const [userInput, setUserInput] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    {
+      title: '',
+      description: '',
+      price: 0,
+      continent: 1,
+      image: [],
+    }
+  );
+
+  const handleChange = (event) => {
+    setUserInput({ [event.target.name]: event.target.value });
+  };
+
+  const handleImage = (newImages) => {
+    setUserInput({ image: newImages });
+  };
+
+  const handleSubmit = () => {
+    if (!title || !price || !description || !continent || !image === 0) {
+      return alert('Complete the form!');
+    }
+
+    const body = {
+      userId: props.user.userData._id,
+      title,
+      price,
+      description,
+      continent,
+      image,
+    };
+    console.log(body);
+
+    axios.post('/api/products', body).then((response) => {
+      if (response.data.success) {
+        alert('Successfuly submitted!');
+        props.history.push('/');
+      } else {
+        alert('Failed to submit!');
+      }
+    });
+  };
+
+  const { title, price, description, continent, image } = userInput;
+  return (
+    <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+      <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+        <Title level={2}>Upload Product</Title>
+        <FileUpload handleImage={handleImage} />
+      </div>
+      <Form onFinish={handleSubmit}>
+        <br />
+        <br />
+        <label>Title</label>
+        <Input onChange={handleChange} type="text" name="title" value={title} />
+        <br />
+        <br />
+        <label>Description</label>
+        <TextArea
+          onChange={handleChange}
+          type="text"
+          name="description"
+          value={description}
+        />
+        <br />
+        <br />
+        <label>Price</label>
+        <Input
+          onChange={handleChange}
+          type="number"
+          name="price"
+          value={price}
+        />
+        <br />
+        <br />
+        <select onChange={handleChange} value={continent} name="continent">
+          {Continents.map((c) => (
+            <option key={c.key} value={c.key}>
+              {c.value}
+            </option>
+          ))}
+        </select>
+        <br />
+        <br />
+        <Button type="primary" htmlType="submit">
+          Upload
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
 export default UploadProductPage;
