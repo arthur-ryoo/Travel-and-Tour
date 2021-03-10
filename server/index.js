bd74903fad0572a10ee4ef835722afc543361bc0
ArthurRyoo <jene0523@gmail.com>
2021-03-09 21:57:17 -0800

    Set static file path for production

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 5000;
+
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+
 const cors = require('cors');
 
 const config = require('./config/key');
@@ -28,5 +31,13 @@ db.once('connected', () => {
 });
 
 app.use('/api/users', require('./routes/users'));
+app.use('/api/products', require('./routes/products'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Express app running on port ${port}`));
