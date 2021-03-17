4a46a3f4a7fc8aace647dec9312d2098a4297c8b
ArthurRyoo <jene0523@gmail.com>
2021-03-17 15:18:13 -0700

    Add logout and addToCart actions

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,9 +1,16 @@
 import axios from 'axios';
-import { LOGIN_USER, REGISTER_USER, AUTH_USER } from './types';
+import {
+  LOGIN_USER,
+  LOGOUT_USER,
+  REGISTER_USER,
+  AUTH_USER,
+  ADD_TO_CART,
+} from './types';
+import { USER_SERVER } from '../Config';
 
 export function loginUser(dataToSubmit) {
   const request = axios
-    .post('/api/users/login', dataToSubmit)
+    .post(`${USER_SERVER}/login`, dataToSubmit)
     .then((response) => response.data);
   return {
     type: LOGIN_USER,
@@ -11,9 +18,20 @@ export function loginUser(dataToSubmit) {
   };
 }
 
+export function logoutUser() {
+  const request = axios
+    .get(`${USER_SERVER}/logout`)
+    .then((response) => response.data);
+
+  return {
+    type: LOGOUT_USER,
+    payload: request,
+  };
+}
+
 export function registerUser(dataToSubmit) {
   const request = axios
-    .post('/api/users/register', dataToSubmit)
+    .post(`${USER_SERVER}/register`, dataToSubmit)
     .then((response) => response.data);
   return {
     type: REGISTER_USER,
@@ -21,9 +39,22 @@ export function registerUser(dataToSubmit) {
   };
 }
 
+export function addToCart(id) {
+  let body = {
+    productId: id,
+  };
+  const request = axios
+    .post(`${USER_SERVER}/addToCart`, body)
+    .then((response) => response.data);
+  return {
+    type: ADD_TO_CART,
+    payload: request,
+  };
+}
+
 export function auth() {
   const request = axios
-    .get('/api/users/auth')
+    .get(`${USER_SERVER}/auth`)
     .then((response) => response.data);
   return {
     type: AUTH_USER,
--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,13 +1,29 @@
-import { LOGIN_USER, REGISTER_USER, AUTH_USER } from '../_actions/types';
+import {
+  LOGIN_USER,
+  LOGOUT_USER,
+  REGISTER_USER,
+  AUTH_USER,
+  ADD_TO_CART,
+} from '../_actions/types';
 
 export default function userReducer(state = {}, action) {
   switch (action.type) {
     case LOGIN_USER:
       return { ...state, loginSuccess: action.payload };
+    case LOGOUT_USER:
+      return { ...state };
     case REGISTER_USER:
       return { ...state, register: action.payload };
     case AUTH_USER:
       return { ...state, userData: action.payload };
+    case ADD_TO_CART:
+      return {
+        ...state,
+        userData: {
+          ...state.userData,
+          cart: action.payload,
+        },
+      };
     default:
       return state;
   }
