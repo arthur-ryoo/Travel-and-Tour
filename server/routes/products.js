308275f22d525acf8ab181ec56a0707c68967834
ArthurRyoo <jene0523@gmail.com>
2021-03-18 20:51:05 -0700

    Add router for product detail

--- server/routes/products.js
+++ server/routes/products.js
@@ -4,5 +4,5 @@ const productsController = require('../controllers/products');
 router.post('/image/new', productsController.uploadImage);
 router.post('/new', productsController.uploadProduct);
 router.get('/', productsController.getAllProducts);
-
+router.get('/:id', productsController.getProduct);
 module.exports = router;
