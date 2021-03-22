25f53cedb4acc24523f7031d1abaf58db000dabd
ArthurRyoo <jene0523@gmail.com>
2021-03-22 12:37:12 -0700

    Fix config for heroku deployment

--- server/index.js
+++ server/index.js
@@ -35,7 +35,8 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/products', require('./routes/products'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  // app.use(express.static('client/build'));
+  app.use(express.static(path_join(__dirname, '../client/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
