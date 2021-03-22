d0768a71514b9715b206ffd7c19253190f030a51
ArthurRyoo <jene0523@gmail.com>
2021-03-22 15:41:42 -0700

    Heroku

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/products', require('./routes/products'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(__dirname + '/client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
