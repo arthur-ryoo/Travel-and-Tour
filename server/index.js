56d19f532107b1c4f0b2b6d52dfc0104f5279221
ArthurRyoo <jene0523@gmail.com>
2021-03-22 18:08:41 -0700

    Heroku

--- server/index.js
+++ server/index.js
@@ -35,15 +35,15 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/products', require('./routes/products'));
 
 if (process.env.NODE_ENV === 'production') {
-  // app.use(express.static('client/build'));
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
-  // });
-  const root = require('path').join(__dirname, '../client', 'build');
-  app.use(express.static(root));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile('index.html', { root });
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
+  // const root = require('path').join(__dirname, '../client', 'build');
+  // app.use(express.static(root));
+  // app.get('*', (req, res) => {
+  //   res.sendFile('index.html', { root });
+  // });
 }
 
 app.listen(port, () => console.log(`Express app running on port ${port}`));
