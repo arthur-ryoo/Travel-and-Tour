91f43cc636e222cf8e2d74999cf2d9c4af4cfad3
ArthurRyoo <jene0523@gmail.com>
2021-03-22 17:47:13 -0700

    Fix bug

--- client/src/components/views/LandingPage/Sections/RadioBox.js
+++ client/src/components/views/LandingPage/Sections/RadioBox.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Collapse, Radio } from 'antd';
 
 const { Panel } = Collapse;
 
 function RadioBox(props) {
+  const [value, setValue] = useState('-createdAt');
+
   const handleChange = (event) => {
+    setValue(event.target.value);
     props.onChange(event.target.value);
   };
 
@@ -12,7 +15,7 @@ function RadioBox(props) {
     <div>
       <Collapse defaultActiveKey={['0']}>
         <Panel header="Sort By" key="1">
-          <Radio.Group onChange={handleChange} value="-createdAt">
+          <Radio.Group onChange={handleChange} value={value}>
             <Radio value="-createdAt">Newest</Radio>
             <Radio value="createdAt">Oldest</Radio>
             <Radio value="-price">Price (highest first)</Radio>
