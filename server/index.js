2b67544934a22b3d957cb27fb52088a8cc4b2ccc
ArthurRyoo <jene0523@gmail.com>
2021-03-22 18:02:05 -0700

    Heroku

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+// const path = require('path');
 require('dotenv').config();
 const port = process.env.PORT || 8080;
 
@@ -35,9 +35,14 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/products', require('./routes/products'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  // app.use(express.static('client/build'));
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  // });
+  const root = require('path').join(__dirname, '../client', 'build');
+  app.use(express.static(root));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    res.sendFile('index.html', { root });
   });
 }
 
